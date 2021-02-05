6e883c38b1b27a3c34af66ebdaff1f8a1c10b484
simpolism <simpolism@gmail.com>
2021-02-05 15:29:15 -0500

    Simplify loading dramatically, use own stylesheet.

--- js/pull.js
+++ js/pull.js
@@ -1,19 +1,3 @@
-/*
-Format of post:
-<div class="author-post single-post-show">
-  <div class="post-content ">
-    <div class="post-header">
-      <h2 class="post-title">
-        <a class="post-title" href="[link-to-post]">[Title]</a>
-      </h2>
-      <div class="post-date">March 5, 2019</div>
-    </div>
-    <div class="post-body">
-      body contents
-    </div>
-  </div>
-</div>
-*/
 async function queryBlog() {
   // TODO: error handling
   const blogResponse = await fetch('https://listed.to/@simpolism');
@@ -22,25 +6,11 @@ async function queryBlog() {
   const domparser = new DOMParser()
   const blogDoc = domparser.parseFromString(blogText, 'text/html');
 
-  // locate and insert stylesheet
-  const links = [ ...blogDoc.getElementsByTagName('link') ];
-  const stylesheetLink = links.find((link) => {
-    const rel = link.attributes.getNamedItem('rel');
-    return rel.value === 'stylesheet';
-  });
-  console.log(stylesheetLink);
-
-  // parse stylesheet url to add explicit domain
-  const stylesheetUrl = stylesheetLink.attributes.getNamedItem('href').value;
-  stylesheetLink.setAttribute('href', `https://listed.to${stylesheetUrl}`);
-  document.head.appendChild(stylesheetLink);
-
   // locate posts and insert their headers
   const posts = [ ...blogDoc.getElementsByClassName('post-header') ];
-  document.body.innerHTML = '<h1>Simpolism <a href="https://listed.to/@simpolism">Listed Blog</a> Master Index</h1>';
-  for (const post of posts) {
-    document.body.appendChild(post)
-  }
+  const loadingText = document.getElementById('loading');
+  document.body.removeChild(loadingText);
+  document.body.append(...posts);
 }
 
 queryBlog();

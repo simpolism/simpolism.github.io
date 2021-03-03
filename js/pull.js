47a6febb70fbbdd304f1ff72788a58659689d112
simpolism <simpolism@gmail.com>
2021-03-03 17:06:12 -0500

    Deduplicate posts.

--- js/pull.js
+++ js/pull.js
@@ -8,9 +8,26 @@ async function queryBlog() {
 
   // locate posts and insert their headers
   const posts = [ ...blogDoc.getElementsByClassName('post-header') ];
+  
+  // dedup posts (had a weird double publish for one of them)
+  const dedupedPosts = { };
+  for (const post of posts) {
+    try {
+      const title = post.childNodes[0].childNodes[0].textContent;
+      if (!dedupedPosts[title]) {
+        dedupedPosts[title] = post;
+      } else {
+        const date = post.childNodes[1].textContent;
+        console.log(`Skipping duplicate post "${title}" (${date}).`);
+      }
+    } catch (e) {
+      console.error('Failed to locate title for post:', post);
+    }
+  }
+
   const loadingText = document.getElementById('loading');
   document.body.removeChild(loadingText);
-  document.body.append(...posts);
+  document.body.append(...Object.values(dedupedPosts));
 }
 
 queryBlog();
